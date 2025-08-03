1ff580b071d7249be7d8ed68ad8ce9d1df2ef5a5
samuel-udoh <chukwuemekasam16@gmail.com>
2025-08-03 17:09:26 +0100

    restructure

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -2,6 +2,11 @@ from .serializers import ListingSerializer, BookingSerializer
 from rest_framework import viewsets
 from .models import Listing, Booking
 
+
+# TODO
+# Add perform Create
+# Create user model for listing and review model
+# Update the seed file
 class ListingViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows listings to be viewed or edited.
@@ -9,8 +14,12 @@ class ListingViewSet(viewsets.ModelViewSet):
     serializer_class = ListingSerializer
     queryset = Listing.objects.all()
 
+    # def perform_create(self, serializer):
+    #     serializer.save(host=self.request.user)
+
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
-
+    # def perform_create(self, serializer):
+    #     serializer.save(user=self.request.user)
